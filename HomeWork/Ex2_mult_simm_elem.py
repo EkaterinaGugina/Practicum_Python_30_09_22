2dcb68ed3ecb0b3bb4344a96f0fcfc75d87619e0
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-30 02:38:11 +0500

    solved Ex2 Practicum 30_09

--- HomeWork/Ex2_mult_simm_elem.py
+++ HomeWork/Ex2_mult_simm_elem.py
@@ -2,3 +2,12 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16]
 # - [2, 3, 5, 6] => [12, 15]
+
+from random import randrange
+n = int(input('Введите число элементов n = '))
+array_1 = [randrange(1, 10) for i in range(n)]
+array_2 = [0]*(n // 2)
+for i in  range(len(array_2)):
+   array_2[i] = array_1[i] * array_1[len(array_1) - 1 - i]
+print(f'{array_1} ---> {array_2}')
+
