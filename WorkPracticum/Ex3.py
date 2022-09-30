25ec392e4a41659e9dfcb35b0aca61056f4bb7be
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-30 23:40:43 +0500

    update WorkPracticum

--- WorkPracticum/Ex3.py
+++ WorkPracticum/Ex3.py
@@ -1,5 +1,4 @@
 # Определить, присутствует ли в заданном списке строк, некоторое число
-str1 = ['воп23', 'д1ы', '56', 'ьу', '0', '8', 'ло765п', '2', 'вк']
 # def find_digit(new_text):
 #     for i in range(len(new_text)):
 #         if new_text[i].isdigit():
@@ -12,5 +11,7 @@ def find_digit(new_text, num):
         if new_text[i].isdigit():
             if int(new_text[i]) == num:
                 print(i)
+str1 = ['воп23', 'д1ы', '56', 'ьу', '0', '8', 'ло765п', '2', 'вк']
+num = 8
 find_digit(str1, num)
 
