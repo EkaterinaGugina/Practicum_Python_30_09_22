1680172bc4bf5724f3987272aa0b524c6b481bc0
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-10-01 00:01:44 +0500

    update HomeWork Ex2

--- HomeWork/Ex2_mult_simm_elem.py
+++ HomeWork/Ex2_mult_simm_elem.py
@@ -6,7 +6,9 @@
 from random import randrange
 n = int(input('Введите число элементов n = '))
 array_1 = [randrange(1, 10) for i in range(n)]
-array_2 = [0]*(n // 2)
+if n % 2 == 0:
+   array_2 = [0]*(n // 2)
+else: array_2 = [0]*(n // 2 + 1)
 for i in  range(len(array_2)):
    array_2[i] = array_1[i] * array_1[len(array_1) - 1 - i]
 print(f'{array_1} ---> {array_2}')
